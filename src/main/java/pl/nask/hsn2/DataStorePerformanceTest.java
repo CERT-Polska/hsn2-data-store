38ecafed3b8524ac6b11c17512706345e05a8d38
Michal Michalak <michal.michalak@nask.pl>
2013-03-19 15:48:00 +0100

    Add cmd line parameters to test.

--- src/main/java/pl/nask/hsn2/DataStorePerformanceTest.java
+++ src/main/java/pl/nask/hsn2/DataStorePerformanceTest.java
@@ -13,11 +13,15 @@ import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.HelpFormatter;
+import org.apache.commons.cli.OptionBuilder;
+import org.apache.commons.cli.Options;
+import org.apache.commons.cli.ParseException;
+import org.apache.commons.cli.PosixParser;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import pl.nask.hsn2.ResourceException;
-import pl.nask.hsn2.StorageException;
 import pl.nask.hsn2.bus.api.Destination;
 import pl.nask.hsn2.bus.api.Message;
 import pl.nask.hsn2.bus.operations.JobFinished;
@@ -38,15 +42,6 @@ import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 
 public class DataStorePerformanceTest {
-	private static final int HOW_MANY_FILES = 100000;
-	private static final long JOB_ID = 123456789;
-	private static final int THREADS_NUMBER = 10;
-	private static final int SMALL_FILE_SIZE = 10000000;
-	private static final int BIG_FILE_SIZE = 35000000;
-	private static final int STRING_FILES_NUMBER = 50;
-	private static final int SMALL_FILES_NUMBER = 10;
-	private static final int BIG_FILES_NUMBER = 1;
-
 	private static final DataStoreConnector DS_CONN = new DataStoreConnectorImpl("http://127.0.0.1:8080/");
 	private static final String RBT_HOST_NAME = "localhost";
 	private static final String RBT_NOTIFY_EXCH_NAME = "notify";
@@ -55,33 +50,13 @@ public class DataStorePerformanceTest {
 
 	private static final String TEST_TEXT = "This is long text. This is long text. This is long text. ";
 	private static final int TEST_TEXT_LENGTH = TEST_TEXT.length();
-	private static final String STRING_FOR_DATA_STORE;
-	private static final Path BIG_FILE;
-	private static final Path SMALL_FILE;
+	private static String STRING_FOR_DATA_STORE;
+	private static Path BIG_FILE;
+	private static Path SMALL_FILE;
 	private static final Logger LOGGER = LoggerFactory.getLogger(DataStorePerformanceTest.class);
-	private static final MessageSerializer<Operation> DEFAULT_SERIALIZER = new ProtoBufMessageSerializer();
-	private static final ExecutorService EXECUTOR = Executors.newFixedThreadPool(THREADS_NUMBER);
-
-	static {
-		// Prepare test String.
-		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < 100; i++) {
-			sb.append(TEST_TEXT);
-		}
-		STRING_FOR_DATA_STORE = sb.toString();
-
-		try {
-			// Prepare big file.
-			BIG_FILE = Files.createTempFile("hsn2-data-store-perfTest-", "");
-			createTempFile(BIG_FILE, BIG_FILE_SIZE);
-
-			// Prepare small file.
-			SMALL_FILE = Files.createTempFile("hsn2-data-store-perfTest-", "");
-			createTempFile(SMALL_FILE, SMALL_FILE_SIZE);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
+	private static MessageSerializer<Operation> DEFAULT_SERIALIZER;
+	private static ExecutorService EXECUTOR;
+	private static long jobId;
 
 	private static void createTempFile(Path file, int fileSize) throws IOException {
 		try (BufferedWriter bw = new BufferedWriter(new FileWriter(file.toFile()))) {
@@ -93,15 +68,25 @@ public class DataStorePerformanceTest {
 		}
 	}
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		// Init.
-		long time = System.currentTimeMillis();
-		CountDownLatch latch = new CountDownLatch(2 * HOW_MANY_FILES);
-		ActionType nextAction = ActionType.STRING;
+		CmdLineOpt cmdLineOpt = new CmdLineOpt(args);
+		prepareTempFiles(cmdLineOpt);
+		DEFAULT_SERIALIZER = new ProtoBufMessageSerializer();
+		EXECUTOR = Executors.newFixedThreadPool(cmdLineOpt.getThreadsNumber());
+		CountDownLatch latch = new CountDownLatch(2 * cmdLineOpt.getHowManyFiles());
+		LOGGER.info(
+				"\nStarting with options:\n- files to test = {}\n- threads number = {}\n- job id = {}\n- big file size = {}\n"
+						+ "- small file size = {}\n- strings per iteration = {}\n- small files per iteration = {}\n- big files per iteration = {}\n",
+				new Object[] { cmdLineOpt.getHowManyFiles(), cmdLineOpt.getThreadsNumber(), cmdLineOpt.getJobId(),
+						cmdLineOpt.getBigFileSize(), cmdLineOpt.getSmallFileSize(), cmdLineOpt.getStringFilesLimit(),
+						cmdLineOpt.getSmallFilesLimit(), cmdLineOpt.getBigFilesLimit() });
 
 		// Submit all tasks.
+		ActionType nextAction = ActionType.STRING;
 		int filesCounter = 0;
-		for (int i = 0; i < HOW_MANY_FILES; i++) {
+		long time = System.currentTimeMillis();
+		for (int i = 0; i < cmdLineOpt.getHowManyFiles(); i++) {
 			// Submit add data task. (Which will run receive data task when finished.)
 			EXECUTOR.execute(new AddDataTask(nextAction, latch));
 			filesCounter++;
@@ -131,33 +116,59 @@ public class DataStorePerformanceTest {
 		EXECUTOR.shutdown();
 	}
 
+	private static void prepareTempFiles(CmdLineOpt cmdLnOpt) {
+		// Prepare test String.
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < 100; i++) {
+			sb.append(TEST_TEXT);
+		}
+		STRING_FOR_DATA_STORE = sb.toString();
+
+		try {
+			// Prepare big file.
+			BIG_FILE = Files.createTempFile("hsn2-data-store-perfTest-", "");
+			createTempFile(BIG_FILE, cmdLnOpt.getBigFileSize());
+
+			// Prepare small file.
+			SMALL_FILE = Files.createTempFile("hsn2-data-store-perfTest-", "");
+			createTempFile(SMALL_FILE, cmdLnOpt.getSmallFileSize());
+		} catch (IOException e) {
+			throw new RuntimeException(e);
+		}
+
+		// Set files limits.
+		ActionType.STRING.setFilesLimit(cmdLnOpt.getStringFilesLimit());
+		ActionType.SMALL_FILE.setFilesLimit(cmdLnOpt.getSmallFilesLimit());
+		ActionType.BIG_FILE.setFilesLimit(cmdLnOpt.getBigFilesLimit());
+	}
+
 	private static enum ActionType {
-		STRING(STRING_FILES_NUMBER) {
+		STRING {
 			@Override
 			public ActionType next() {
 				return SMALL_FILE;
 			}
 		},
-		SMALL_FILE(SMALL_FILES_NUMBER) {
+		SMALL_FILE {
 			@Override
 			public ActionType next() {
 				return BIG_FILE;
 			}
 		},
-		BIG_FILE(BIG_FILES_NUMBER) {
+		BIG_FILE {
 			@Override
 			public ActionType next() {
 				return STRING;
 			}
 		};
 
-		private final int filesLimit;
-
-		private ActionType(int filesLimitCounter) {
-			filesLimit = filesLimitCounter;
-		}
+		private int filesLimit = 1;
 
 		public abstract ActionType next();
+
+		private void setFilesLimit(int filesLimitCounter) {
+			filesLimit = filesLimitCounter;
+		}
 
 		public int getFileLimit() {
 			return filesLimit;
@@ -182,14 +193,14 @@ public class DataStorePerformanceTest {
 				switch (actionToDo) {
 				case STRING:
 					LOGGER.debug("Adding string...");
-					dr = DS_CONN.sendPost(STRING_FOR_DATA_STORE.getBytes(), JOB_ID);
+					dr = DS_CONN.sendPost(STRING_FOR_DATA_STORE.getBytes(), jobId);
 					key = dr.getRef().getKey();
 					type = dr.getType();
 					break;
 				case BIG_FILE:
 					LOGGER.debug("Adding big file...");
 					try (InputStream bigFile = new BufferedInputStream(new FileInputStream(BIG_FILE.toFile()))) {
-						dr = DS_CONN.sendPost(bigFile, JOB_ID);
+						dr = DS_CONN.sendPost(bigFile, jobId);
 						key = dr.getRef().getKey();
 						type = dr.getType();
 					}
@@ -197,7 +208,7 @@ public class DataStorePerformanceTest {
 				case SMALL_FILE:
 					LOGGER.debug("Adding small file...");
 					try (InputStream smallFile = new BufferedInputStream(new FileInputStream(SMALL_FILE.toFile()))) {
-						dr = DS_CONN.sendPost(smallFile, JOB_ID);
+						dr = DS_CONN.sendPost(smallFile, jobId);
 						key = dr.getRef().getKey();
 						type = dr.getType();
 					}
@@ -225,7 +236,7 @@ public class DataStorePerformanceTest {
 		@Override
 		public void run() {
 			try {
-				try (BufferedInputStream bis = new BufferedInputStream(DS_CONN.getResourceAsStream(JOB_ID, referenceId))) {
+				try (BufferedInputStream bis = new BufferedInputStream(DS_CONN.getResourceAsStream(jobId, referenceId))) {
 					StringBuilder sb = new StringBuilder();
 					int chInt;
 					int size = 0;
@@ -261,7 +272,7 @@ public class DataStorePerformanceTest {
 			rbtConnection = connFactory.newConnection();
 			Channel channel = rbtConnection.createChannel();
 			channel.exchangeDeclare(RBT_NOTIFY_EXCH_NAME, "fanout");
-			Operation jobFinishedOperation = new JobFinished(JOB_ID, JobStatus.COMPLETED);
+			Operation jobFinishedOperation = new JobFinished(jobId, JobStatus.COMPLETED);
 			Destination dst = new RbtDestination(RBT_MAIN_EXCHANGE_NAME, "");
 			sendOut(jobFinishedOperation, channel, dst, "");
 			rbtConnection.close();
@@ -293,5 +304,143 @@ public class DataStorePerformanceTest {
 				LOGGER.error("Serialization error.", e);
 			}
 		}
+	}
+
+	private static class CmdLineOpt {
+		private static final int CONSOLE_WIDTH = 110;
+		private final Options options = new Options();
+
+		private static final String HOW_MANY_FILES_DEFAULT = "10";
+		private static final String JOB_ID_DEFAULT = "123456789";
+		private static final String THREADS_NUMBER_DEFAULT = "10";
+		private static final String SMALL_FILE_SIZE_DEFAULT = "8000000";
+		private static final String BIG_FILE_SIZE_DEFAULT = "20000000";
+		private static final String STRING_FILES_LIMIT_DEFAULT = "200";
+		private static final String SMALL_FILES_LIMIT_DEFAULT = "10";
+		private static final String BIG_FILES_LIMIT_DEFAULT = "1";
+
+		private int howManyFiles;
+		private long jobId;
+		private int threadsNumber;
+		private int smallFileSize;
+		private int bigFileSize;
+		private int stringFilesLimit;
+		private int smallFilesLimit;
+		private int bigFilesLimit;
+
+		public CmdLineOpt(String[] args) throws ParseException {
+			initOptions();
+			parseCmdLineArgs(args);
+		}
+
+		private void initOptions() {
+			OptionBuilder.withDescription("Prints this help page.");
+			OptionBuilder.withLongOpt("help");
+			options.addOption(OptionBuilder.create("h"));
+
+			OptionBuilder.withDescription("How many files to test. One PUT and one GET action for each file. (Default: "
+					+ HOW_MANY_FILES_DEFAULT + ")");
+			OptionBuilder.withLongOpt("files");
+			OptionBuilder.hasArgs(1);
+			OptionBuilder.withArgName("number");
+			options.addOption(OptionBuilder.create("f"));
+
+			OptionBuilder.withDescription("Job id. (Default: " + JOB_ID_DEFAULT + ")");
+			OptionBuilder.withLongOpt("job");
+			OptionBuilder.hasArgs(1);
+			OptionBuilder.withArgName("number");
+			options.addOption(OptionBuilder.create("j"));
+
+			OptionBuilder.withDescription("Threads number. (Default: " + THREADS_NUMBER_DEFAULT + ")");
+			OptionBuilder.withLongOpt("threads");
+			OptionBuilder.hasArgs(1);
+			OptionBuilder.withArgName("number");
+			options.addOption(OptionBuilder.create("t"));
+
+			OptionBuilder.withDescription("Small file size in bytes. (Default: " + SMALL_FILE_SIZE_DEFAULT + ")");
+			OptionBuilder.withLongOpt("smallSize");
+			OptionBuilder.hasArgs(1);
+			OptionBuilder.withArgName("number");
+			options.addOption(OptionBuilder.create("ss"));
+
+			OptionBuilder.withDescription("Big file size in bytes. (Default: " + BIG_FILE_SIZE_DEFAULT + ")");
+			OptionBuilder.withLongOpt("bigSize");
+			OptionBuilder.hasArgs(1);
+			OptionBuilder.withArgName("number");
+			options.addOption(OptionBuilder.create("bs"));
+
+			OptionBuilder.withDescription("Strings number per iteration. (Default: " + STRING_FILES_LIMIT_DEFAULT + ")");
+			OptionBuilder.withLongOpt("strPerIter");
+			OptionBuilder.hasArgs(1);
+			OptionBuilder.withArgName("number");
+			options.addOption(OptionBuilder.create("spi"));
+
+			OptionBuilder.withDescription("Small files number per iteration. (Default: " + SMALL_FILES_LIMIT_DEFAULT + ")");
+			OptionBuilder.withLongOpt("smlFilesPerIter");
+			OptionBuilder.hasArgs(1);
+			OptionBuilder.withArgName("number");
+			options.addOption(OptionBuilder.create("sfpi"));
+
+			OptionBuilder.withDescription("Big files number per iteration. (Default: " + BIG_FILES_LIMIT_DEFAULT + ")");
+			OptionBuilder.withLongOpt("bigFilesPerIter");
+			OptionBuilder.hasArgs(1);
+			OptionBuilder.withArgName("number");
+			options.addOption(OptionBuilder.create("bfpi"));
+		}
+
+		private void parseCmdLineArgs(String[] args) throws ParseException {
+			// Parse arguments.
+			CommandLine cmd = new PosixParser().parse(options, args);
+
+			// Check for options.
+			if (cmd.hasOption("help")) {
+				HelpFormatter formatter = new HelpFormatter();
+				formatter.setWidth(CONSOLE_WIDTH);
+				formatter.printHelp("java -jar ...", options);
+				System.exit(0);
+			} else {
+				howManyFiles = Integer.valueOf(cmd.getOptionValue("f", HOW_MANY_FILES_DEFAULT));
+				jobId = Long.valueOf(cmd.getOptionValue("j", JOB_ID_DEFAULT));
+				threadsNumber = Integer.valueOf(cmd.getOptionValue("t", THREADS_NUMBER_DEFAULT));
+				smallFileSize = Integer.valueOf(cmd.getOptionValue("ss", SMALL_FILE_SIZE_DEFAULT));
+				bigFileSize = Integer.valueOf(cmd.getOptionValue("bs", BIG_FILE_SIZE_DEFAULT));
+				stringFilesLimit = Integer.valueOf(cmd.getOptionValue("spi", STRING_FILES_LIMIT_DEFAULT));
+				smallFilesLimit = Integer.valueOf(cmd.getOptionValue("sfpi", SMALL_FILES_LIMIT_DEFAULT));
+				bigFilesLimit = Integer.valueOf(cmd.getOptionValue("bfpi", BIG_FILES_LIMIT_DEFAULT));
+			}
+		}
+
+		public int getHowManyFiles() {
+			return howManyFiles;
+		}
+
+		public long getJobId() {
+			return jobId;
+		}
+
+		public int getThreadsNumber() {
+			return threadsNumber;
+		}
+
+		public int getSmallFileSize() {
+			return smallFileSize;
+		}
+
+		public int getBigFileSize() {
+			return bigFileSize;
+		}
+
+		public int getStringFilesLimit() {
+			return stringFilesLimit;
+		}
+
+		public int getSmallFilesLimit() {
+			return smallFilesLimit;
+		}
+
+		public int getBigFilesLimit() {
+			return bigFilesLimit;
+		}
+
 	}
 }
